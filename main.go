377787b031c8d89decf6baaf535bf66d858db80a
ahmedharabi <harabiahmed88@gmail.com>
2025-08-02 22:05:42 +0100

    fix: commands executing on the main os

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("/bin/sh")
+	if len(os.Args) > 1 && os.Args[1] == "child" {
+		runChild()
+		return
+	}
+
+	cmd := exec.Command("/proc/self/exe", "child")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -18,13 +23,33 @@ func main() {
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWIPC,
-		Chroot: "./rootfs",
 	}
-
-	cmd.Dir = "/"
 
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Error: %v", err)
+	}
+}
+
+func runChild() {
+
+	if err := syscall.Sethostname([]byte("mycontainer")); err != nil {
+		log.Fatalf("failed to set hostname %v", err)
+	}
+
+	if err := syscall.Chroot("./rootfs"); err != nil {
+		log.Fatalf("Failed to chroot: %v", err)
+	}
+	if err := os.Chdir("/"); err != nil {
+		log.Fatalf("Failed to change dir: %v", err)
+	}
+	if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
+		log.Fatalf("Failed to mount /proc: %v", err)
+	}
+	env := append(os.Environ(), "PS1=\\u@\\h:\\w\\$ ")
+	syscall.Exec("/bin/sh", []string{"/bin/sh"}, env)
+
+	if err := syscall.Exec("/bin/sh", []string{"/bin/sh"}, env); err != nil {
+		log.Fatalf("Failed to exec shell: %v", err)
 	}
 
 }
