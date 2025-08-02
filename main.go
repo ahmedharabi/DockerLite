b12b748ed96987ba8d435811156aa081988cf8e4
ahmedharabi <harabiahmed88@gmail.com>
2025-08-02 18:13:48 +0100

    feat: add namespace isolation

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWIPC,
+		Chroot: "./rootfs",
+	}
+
+	cmd.Dir = "/"
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+}
